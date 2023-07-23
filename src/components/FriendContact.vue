82ec4f7655357403a00d826bf793d376ba88b7e9
BizarrIlliada <kazkoviy@icloud.com>
2023-07-23 15:15:09 +0300

    task is done

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="showFriendsDetails">
+      {{ areDetailsVisible ? 'Hide' : 'Show' }} details
+    </button>
+    <ul v-if="areDetailsVisible">
+      <li>
+        Phone: {{ friend.phone }}
+      </li>
+      <li>
+        Email: {{ friend.email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'FriendContact',
+    props: ['friend'],
+
+    data() {
+      return {
+        areDetailsVisible: false,
+      }
+    },
+
+    methods: {
+      showFriendsDetails() {
+        this.areDetailsVisible = !this.areDetailsVisible;
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import { createApp } from 'vue';
 
-createApp({}).mount('');
+import App from './App.vue';
+import FriendContact from './components/FriendContact.vue';
+
+const components = [
+  FriendContact,
+]
+
+const app = createApp(App);
+
+components.forEach(comp => {
+  app.component(comp.name, comp)
+})
+
+app.mount('#app');
